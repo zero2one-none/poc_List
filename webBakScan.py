38dc467f081111d4fcb5ebf27ade7f3356c22016
zero2one-none <68674733+zero2one-none@users.noreply.github.com>
2021-11-10 17:40:13 +0800

    Create webBakScan.py

---
+++ webBakScan.py
@@ -0,0 +1,23 @@
+'''
+网站备份文件泄露
+目前需要手动更改变量 target，因为我还不会用sys库传参，见凉
+背景：CTFHub =》 技能树 =》 信息泄露 =》 网站备份文件
+'''
+import requests
+
+# 常用备份文件名
+files = ['web', 'website', 'backup', 'back', 'www', 'wwwroot', 'temp']
+
+# 常用备份文件类型
+suffixes = ['tar', 'tar.gz', 'zip', 'rar']
+
+
+for file in files:
+    for suffix in suffixes:
+        target = ''
+        bak = file + '.' + suffix
+        url = target + bak
+        # print(url)
+        resp = requests.get(url=url)
+        print(bak, ' : ', resp.status_code)
+        resp.close()
